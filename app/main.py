84fe9f85c247f4d0ad1ec2baed70226136021238
Jay Surya <raj.jaysurya04@gmail.com>
2024-12-01 22:22:23 +0530

    corrected path

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routers.students import router as student_router
-from app.DB.mongoDB import MongoDB
+from .routers.students import router as student_router
+from .DB.mongoDB import MongoDB
 import os
 
 app= FastAPI()
